b6d28e0a47199a64bb6ac212f345d6a0797ef7ef
Bertrand Delacretaz <bdelacretaz@apache.org>
2023-10-04 16:46:07 +0200

    Fix p-test

--- blocks/float/float.js
+++ blocks/float/float.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
   const ptest = document.createElement('p-test');
-  ptest-setAttribute('marker', "42");
+  ptest.setAttribute('marker', "42");
   block.append(ptest);
 }
